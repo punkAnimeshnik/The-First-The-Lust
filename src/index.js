ca0f989c8bbda9c1574473868eb6e5995797855e
ilia <50945278+punkAnimeshnik@users.noreply.github.com>
2021-04-15 22:34:13 +0300

    Подключение дополнительно шрифта HelveticaNevueCry, сделал набросок шапки огловления и выпадающих списков.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
 import '@fortawesome/fontawesome-free/js/all'
 import './less/main.less'
-import 'bootstrap'
 import './babel'
-import Vue from 'vue'
-
-var app = new Vue({
-    el: '#app',
-    data: {
-        message: 'Тестовый текст для проверки Vue'
-    }
-})
+import './js/vue'
