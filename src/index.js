985fbe75d535769c7a841b68e8d8bf09b32b0865
ilia <50945278+punkAnimeshnik@users.noreply.github.com>
2021-04-16 18:31:03 +0300

    Сверстал внешнюю оболочку сайта

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import '@fortawesome/fontawesome-free/js/all'
 import './less/main.less'
 import './babel'
 import './js/vue'
+
--- src/js/vue.js
+++ src/js/vue.js
@@ -3,6 +3,56 @@ import Vue from 'vue'
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Тестовый текст для проверки Vue!!!'
+        peoples: [
+            {
+                img: "../src/img/foto/Luke Skywalker.jpg",
+                name: "Luke Skywalker",
+                eye: "blue",
+                height: 172,
+                mass: 77,
+                age: "19BBY"
+            },
+            {
+                img: "../src/img/foto/C-3PO.png",
+                name: "C-3PO",
+                eye: "yellow",
+                height: 167,
+                mass: 75,
+                age: "112BBY"
+            },
+            {
+                img: "../src/img/foto/Luke Skywalker.jpg",
+                name: "Luke Skywalker",
+                eye: "blue",
+                height: 172,
+                mass: 77,
+                age: "19BBY"
+            },
+            {
+                img: "../src/img/foto/C-3PO.png",
+                name: "C-3PO",
+                eye: "yellow",
+                height: 167,
+                mass: 75,
+                age: "112BBY"
+            },
+            {
+                img: "../src/img/foto/Luke Skywalker.jpg",
+                name: "Luke Skywalker",
+                eye: "blue",
+                height: 172,
+                mass: 77,
+                age: "19BBY"
+            },
+            {
+                img: "../src/img/foto/C-3PO.png",
+                name: "C-3PO",
+                eye: "yellow",
+                height: 167,
+                mass: 75,
+                age: "112BBY"
+            },
+
+        ]
     }
 })
